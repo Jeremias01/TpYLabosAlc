0f7461a7d918aafdbb7a1511b24b430460829313
Jeremias01 <147272167+Jeremias01@users.noreply.github.com>
2025-11-04 14:12:26 -0300

    Update labo08_SVD.py

--- src/labo08_SVD.py
+++ src/labo08_SVD.py
@@ -5,5 +5,25 @@ def svd_reducida(A, k = "max", tol = 1e-15):
     tol la tolerancia para considerar un valor singular igual a cero
     Retorna hatU (matriz de m x k),hatSig (vector de k valores singulares) y hatV (matriz de n x k)
     """
-    raise NotImplementedError("Implementar")
+
+    matriz=matmul(traspuesta(A), A)
+
+    hatV, Avals=diagRH(matriz)
+
+    hatSig=[]
     
+    for i in range(len(Avals)):
+        if(Avals[i][i]>=tol and i<=k):
+            hatSig.append=sqr(Avals[i][i])
+    if(len(hatSig)<k):
+        for i in range(k-len(hatSig)):
+            hatSig.append(0)
+
+    
+
+    hatU= normalize(matmul(A, hatV))
+
+    return hatU, hatSig, hatV
+
+
+    # No esta testeada
