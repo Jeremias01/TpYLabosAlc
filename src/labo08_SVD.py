06eccd20319964566e5c40cda0d494ddecf2a6c7
Your Name <you@example.com>
2025-11-11 14:03:31 -0300

    labo 8 cosas

--- src/labo08_SVD.py
+++ src/labo08_SVD.py
@@ -1,3 +1,19 @@
+
+import sys
+
+sys.path.append(".") 
+sys.path.append("../src") 
+sys.path.append("./src") 
+from labo00_auxiliares import *
+from labo01_errores_igualdad import *
+from labo02_TLs_basicas import *
+from labo03_normas import *
+from labo04_LU import *
+from labo05_QR import *
+from labo06_AVs import *
+from labo07_markov import *
+from labo08_SVD import *
+### TODOS ESTOS IMPORTS SE VAN A TENER QUE REEMPLAZAR CON EL CODIGO ENTERO EN LA ENTREGA FINAL TODO
 import numpy as np
 import copy
 
@@ -9,34 +25,45 @@ def svd_reducida(A, k = "max", tol = 1e-15):
     Retorna hatU (matriz de m x k),hatSig (vector de k valores singulares) y hatV (matriz de n x k)
     """
 
+    if(k=="max"):
+        k=len(A)
     matriz=matmul(traspuesta(A), A)
 
-    hatV, Avals=diagRH(matriz)
+    Avects, Avals=diagRH(matriz)
 
-    hatSig=[]
-    
+    hatSig=np.zeros(k)
+    contador=0
     for i in range(len(Avals)):
-        if(Avals[i][i]>=tol and i<=k):
-            hatSig.append=sqr(Avals[i][i])
-    if(len(hatSig)<k):
+        if(Avals[i][i]>=tol and i<k):
+            hatSig[contador]=np.sqrt(Avals[i][i])
+            contador+=1
+    if(contador<k):
         for i in range(k-len(hatSig)):
-            hatSig.append(0)
+            hatSig[i]=0
+            
+    hatV=np.zeros((len(A[0]),k))
+    for i in range(k):
+        hatV[:,i] = Avects[i]
 
-    
-
-    hatU= normalize(matmul(A, hatV))
+    hatU=matmul(A,hatV)/norma(matmul(A,hatV),2)
 
     return hatU, hatSig, hatV
 
     # No esta testeada
 
+a=[[1,2,3],[4,5,6],[7,8,9]]
+b=np.zeros((3,3))
+for i in range(len(b)):
+    b[i,:] = a[i]
+print(b)
+
 
 def svd_completa(A, tol = 1e-15):
-    V,Avals=diagRH(A,tol) # matriz de autovect y de avals
+    V,Avals=diagRH(matmul(traspuesta(A),A),tol) # matriz de autovect y de avals
     Sigma=[]
     for i in range(len(Avals)):
         if(Avals[i][i]>=tol):   
-            Sigma.append=sqr(Avals[i][i])   # asigno a sigma sus valores singulares
+            Sigma.append=np.sqrt(Avals[i][i])   # asigno a sigma sus valores singulares
     B=matmul(A,V)   
     normalizado=[]
     suma=np.zeros(len(B[0]))
@@ -46,11 +73,12 @@ def svd_completa(A, tol = 1e-15):
     while(len(normalizado)<len(B)):       # hasta que normalizado esté completo
         canonica=np.zeros(len(normalizado))
         for i in range(len(normalizado),len(B)):    
-            if(norma(normalizado[i-len(normalizado)],2)!=0):
-                canonica[i-len(normalizado)]=1      # creo el canonico
-                suma=suma+proyectar(canonica,normalizado[i-len(normalizado)])       # voy sumando las proyecciones del canonico en los vectores del espacio ortonormal hasta ahora
-                normalizado=normalizado.append(normalizado[i-len(normalizado)]-suma)        # agrego un vector ortonormal
-                canonica[i-len(normalizado)]=0      # reinicio a ceros el canonico
+            if(norma(normalizado[len(normalizado)-i],2)!=0):
+                canonica[len(normalizado)-i]=1      # creo el canonico
+                suma=suma+proyectar(canonica,normalizado[len(normalizado)-i])       # voy sumando las proyecciones del canonico en los vectores del espacio ortonormal hasta ahora
+                normalizado=normalizado.append(normalizado[len(normalizado)-i]-suma)        # agrego un vector ortonormal
+                canonica[len(normalizado)-i]=0      # reinicio a ceros el canonico
+                
 
     U=normalizado       #cambio de nombre para el return
     return U,Sigma,V
