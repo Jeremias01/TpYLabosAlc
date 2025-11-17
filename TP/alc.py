a0e5d9b5ad5a41b1f69a61a225d65d516273cc01
Jeremias01 <jere7005@gmail.com>
2025-11-17 00:46:22 -0300

    nosee

--- TP/alc.py
+++ TP/alc.py
@@ -150,7 +150,7 @@ def pinvSVD(U, S, V, Y):
     # Como tenemos S vector que representa una matriz diagonal, podemos hacer mas rápida la multiplicación
     VS_inv = np.zeros(V.shape)
     for i in range(V.shape[1]):
-        VS_inv[:, i] = V[:, i] * S[i]
+        VS_inv[:, i] = V[:, i] * S_inv[i]
     
     A_pseudoinv = matmul(VS_inv, traspuesta(U))
     W = matmul(Y,A_pseudoinv)
